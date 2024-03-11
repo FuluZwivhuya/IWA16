80ffd05566fc7afbc2df42add580aca128eb2715
FuluZwivhuya <148289949+FuluZwivhuya@users.noreply.github.com>
2024-03-11 06:36:31 +0200

    yet to make changes for date and time

--- script.js
+++ script.js
@@ -66,17 +66,40 @@ const MONTHS = [
   const firstName1 = (data["response"]["data"]["NM372"]["firstName"]);
   const surname1 = (data["response"]["data"]["NM372"]["surname"]);
   const id1 = (data["response"]["data"]["NM372"]["id"]);
-  const race1 = (data["response"]["data"]["NM372"]["races"]).length;
-  const EventDate1 = (data["response"]["data"]["NM372"]["races"])
+  const race1 = (data["response"]["data"]["NM372"]["races"]).length;//number of race done 
+  const EventDate1 = (data["response"]["data"]["NM372"]["races"])//yet to retrieve date
 
-  console.log(race1);
+  console.log(EventDate1);//test console
 
   const firstName2 = (data["response"]["data"]["SV782"]["firstName"]);
   const surname2 = (data["response"]["data"]["SV782"]["surname"]);
   const id2 = (data["response"]["data"]["SV782"]["id"])
   const race2 = (data["response"]["data"]["SV782"]["races"]).length;
-  const EventDate2 =(data["response"]["data"]["NM372"]["races"])
+  const EventDate2 =(data["response"]["data"]["NM372"]["races"])//not done yet
   
+  const section1 = document.querySelector('[data-athlete="NM372"]');//first section
+  const h2a = document.createElement('h2');//creating h2
+  const list1 = document.createElement ('dl');//creating dl
+  section1.append(h2a);// inserting h2 in the first section on the html
+  section1.append(list1);//inserting dl in the fisrt section on the html (named it list1)
+  h2a.innerText = id1;//adding text in the h2 created in the fisrt section
+  list1.innerText = // adding information in the list1(dl) created in the fisrt element
+  `Athlete:${firstName1} ${surname1}
+  \n Total Races: ${race1}
+  \n Event Date(Latest):
+  \n Total time:`;
+
+  const section2 =document.querySelector('[data-athlete="SV782"]');
+  const h2b = document.createElement('h2');
+  const list2 = document.createElement('dl');
+  section2.append(h2b);
+  section2.append(list2);
+  h2b.innerText=id2;
+  list2.innerText =
+  `Athlete:${firstName2} ${surname2}
+   \n Total Races: ${race2}
+   \n Event Date(Latest):
+   \n Total time:`;
 
 
   // Only edit below this comment
@@ -90,34 +113,9 @@ const MONTHS = [
   
     const title = document.createElement('h2');
     title.textContent = id;
-    fragment.appendChild(title);*/
-  
-    const section1 = document.querySelector('[data-athlete="NM372"]');
-    const h2a = document.createElement('h2');
-    const list1 = document.createElement ('dl');
-    section1.append(h2a);
-    section1.append(list1);
-    h2a.innerText = id1;
-    list1.innerText = 
-    `Athlete:${firstName1} ${surname1}
-    \n Total Races: ${race1}
-    \n Event Date(Latest):
-    \n Total time:`;
-
-    const section2 =document.querySelector('[data-athlete="SV782"]');
-    const h2b = document.createElement('h2');
-    const list2 = document.createElement('dl');
-    section2.append(h2b);
-    section2.append(list2);
-    h2b.innerText=id2;
-    list2.innerText =
-    `Athlete:${firstName2} ${surname2}
-     \n Total Races: ${race2}
-     \n Event Date(Latest):
-     \n Total time:`;
-
+    fragment.appendChild(title)
     
-   /* const day = date.getUTCDate();
+    const day = date.getUTCDate();
     const month = MONTHS[date.getUTCMonth()];
     const year = date.getUTCFullYear();
   
